32050f2d39946d335c78cfb57e2858be274ba854
Ramello Peralta <ramelloperalta@gmail.com>
2021-05-28 17:44:48 -0400

    Scrapy Project: Reddit Image web scraping
    Extracting images from the front page of reddit/r/food into a html file.

---
+++ RedditFood.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 16:32:01 2021
+
+@author: Ramello
+"""
+
+
+import scrapy
+
+class RedditSpider(scrapy.Spider):
+    name = 'reddit'
+    start_urls = ['https://www.reddit.com/r/food']
+    
+    def parse(self, response):
+        links = response.xpath('//img/@src') #takes link attributes using xpath
+        html = ''
+        
+        for link in links:
+            url = link.get() 
+            
+            #does url have image?
+            if any(extension in url for extension in ['.jpg', '.gif', '.png']):
+                html += f'''<a href='{url}'target='_blank'><img src='{url}'
+                height='33%' width='33%'/><a/>'''
+                
+                with open('frontpage.html', 'a') as page:
+                    page.write(html)
